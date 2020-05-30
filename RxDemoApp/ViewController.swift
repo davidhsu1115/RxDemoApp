c69ef9c26216b17c4db7af1d3bd8f4ab6f25c643
DavidHsu <davidhsu1115@gmail.com>
2020-05-30 16:18:46 +0800

    Initial Commit

---
+++ RxDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxDemoApp
+//
+//  Created by hsu David on 2020/5/30.
+//  Copyright © 2020 hsu David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RxDemoAppTests/RxDemoAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxDemoAppTests.swift
+//  RxDemoAppTests
+//
+//  Created by hsu David on 2020/5/30.
+//  Copyright © 2020 hsu David. All rights reserved.
+//
+
+import XCTest
+@testable import RxDemoApp
+
+class RxDemoAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
